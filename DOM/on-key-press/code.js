78a5631930cb85b62d2c9236e57d039c4ede5471
B1Z0N <kolausf@gmail.com>
2019-09-23 20:52:50 +0300

    chore

--- DOM/on-key-press/code.js
+++ DOM/on-key-press/code.js
@@ -8,7 +8,30 @@ let getRandomColor = () => {
 }
 
 let onKeyPressHandler = () => {
-  document.body.style.background = getRandomColor();
+  var divs = document.getElementsByTagName("div"), i=divs.length;
+  while (i--) {
+    divs[i].setAttribute("style", "background-color: " + getRandomColor() + ";");
+  } 
 }
 
+let createContainer = (parent) => {
+  let container = document.createElement("div");
+  container.className = "container";
+
+  let properties = "width: 75%; height: 75%; display:flex; justify-content:center; align-items:center;";
+  container.style.cssText = properties;
+
+  parent.appendChild(container);
+  return container;
+}
+
+let createNContainers = (n, parent = document.body) => {
+  let container = parent;
+  while(n--) {
+    container = createContainer(container);
+  }
+}
+
+// console.log(document.getElementById("whole-page"))
+createNContainers(10, document.getElementById("whole-page"));
 window.addEventListener("keydown", onKeyPressHandler, true);
